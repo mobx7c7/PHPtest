accad93531ce17c0733a9735c84347f420df1084
Marcelo de Oliveira Bittencourt <mcrmaxx@gmail.com>
2021-05-26 11:31:56 -0300

    Create dotenv initializer tool

---
+++ tools/init_dotenv.php
@@ -0,0 +1,43 @@
+<?php
+const ENV_FILE = '.env';
+
+function getResponseSTDIN($text, $default = '')
+{
+    if (empty($default)) {
+        echo "$text";
+    } else {
+        echo "$text ($default)";
+    }
+
+    echo PHP_EOL;
+
+    echo '> ';
+
+    $response = trim(fgets(STDIN));
+
+    echo PHP_EOL;
+
+    return !empty($response) ? $response : $default;
+}
+
+function writeEnvFile($arr)
+{
+    $file = fopen(ENV_FILE,'w');
+    foreach ($arr as $key => $val)
+    {
+        fwrite($file, "$key=\"$val\"".PHP_EOL);
+    }
+    fclose($file);
+}
+
+echo 'DOTENV INITIALIZER'.PHP_EOL.PHP_EOL;
+
+$env['DB_HOST'] = getResponseSTDIN('Enter with a database host','localhost');
+$env['DB_PORT'] = getResponseSTDIN('Enter with a database port', '3306');
+$env['DB_USER'] = getResponseSTDIN('Enter with a database user', 'root');
+$env['DB_PASS'] = getResponseSTDIN('Enter with a database password','');
+$env['DB_NAME'] = getResponseSTDIN('Enter with a database name','phptest');
+
+writeEnvFile($env);
+
+echo 'File was created.';
